aec6aaea1e1b1545fcfb273c9693aea146583160
srikanta1505 <srikantananda1505@gmail.com>
2025-12-29 23:20:31 +0530

    Added Spring Security

---
+++ src/main/java/practice/todo/user/UserRepository.java
@@ -0,0 +1,10 @@
+package practice.todo.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<AppUser, Long>{
+	
+	Optional<AppUser> findByUsername(String username);
+}
