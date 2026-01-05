9c2d7fbf22428de963101a35e59ca32407481076
srikanta1505 <srikantananda1505@gmail.com>
2026-01-05 23:46:46 +0530

    Auth added

--- src/main/java/practice/todo/exception/GlobalExceptionHandler.java
+++ src/main/java/practice/todo/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -34,6 +35,15 @@ public class GlobalExceptionHandler {
 
         ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+    
+    @ExceptionHandler({UsernameNotFoundException.class})
+    public ResponseEntity<String> handleUsernameNotFoundException(
+            UsernameNotFoundException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
     }
 
 }
